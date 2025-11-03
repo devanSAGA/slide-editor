8e9832d2ea8404571ff143cbf7ad2e299dbbbdd5
Devansh Purohit <officialdevansh@gmail.com>
2025-11-03 16:25:05 +0530

    Initial commit. Setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slide Editor
+
+Building a collaborative slide editor
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="rounded-lg bg-blue-500 p-4 text-white">Slide Editor</div>
+  )
+}
+
+export default App
